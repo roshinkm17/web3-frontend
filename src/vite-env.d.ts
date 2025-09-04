c365e2bc1eda5452cc869f9bfe4f320ea02fc215
Roshin K M <roshin@propertyguru.tech>
2025-09-05 02:39:10 +0530

    fix build error

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,7 @@
 
 declare const __VITE_BACKEND_HOST__: string;
 declare const __VITE_ENVIRONMENT_ID__: string;
+
 interface ImportMetaEnv {
   readonly VITE_BACKEND_HOST: string;
   readonly VITE_ENVIRONMENT_ID: string;
@@ -9,4 +10,10 @@ interface ImportMetaEnv {
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
+}
+
+// CSS module declarations
+declare module '*.css' {
+  const content: string;
+  export default content;
 }
